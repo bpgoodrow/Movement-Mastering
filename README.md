9741f899cd02e83e512b3c5fd4aa17e77d40d56e
Ben Goodrow <bpgoodrow@gmail.com>
2022-10-17 14:36:32 -0700

    intial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Movement Mastering
+
+## Hour Log
+
+- 10/17/2022
+- 2:00 - 3:00: Initial Project
---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import PortfolioControl from "./PortfolioControl";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <PortfolioControl />
+    </div>
+  );
+}
+
+export default App;
