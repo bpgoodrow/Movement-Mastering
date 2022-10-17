3d8f902a8f18181886fcebbdec713b0e87699917
Ben Goodrow <bpgoodrow@gmail.com>
2022-10-17 14:53:56 -0700

    initial push

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import PortfolioControl from "./PortfolioControl";
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <PortfolioControl />
+      <Contact />
     </div>
   );
 }
