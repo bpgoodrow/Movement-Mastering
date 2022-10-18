9948e0fbc10207f2d776cfc81aa66a91f57d4e90
Ben Goodrow <bpgoodrow@gmail.com>
2022-10-18 16:05:41 -0700

    begin portfolio logic

--- src/Components/PortfolioControl.js
+++ src/Components/PortfolioControl.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Portfolio from "./Portfolio";
 import PortfolioForm from "./PortfolioForm";
 import Login from "./Login";
 
+import { db, auth } from './../firebase';
+import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc, QuerySnapshot } from "firebase/firestore";
+
 const PortfolioControl = () => {
+
+  const [portofolio, setPortfolio] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      collection(db, "mastersPortfolio"),
+      (querySnapshot) => {
+        const mastersPortfolio = [];
+        querySnapshot.forEach((doc) => {
+          mastersPortfolio.push({
+            artistName: doc.data().artistName,
+            albumName: doc.data().albumName,
+            songName: doc.data().songName,
+            description: doc.data().description,
+            id: doc.id
+          });
+        });
+        setPortfolio(mastersPortfolio);
+      },
+      (error) => {
+      setError(error.message);
+    }
+    );
+  }, []);
+
   return(
     <div>
       <h2>PortfolioControl</h2>
